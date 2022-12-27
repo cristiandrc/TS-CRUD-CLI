ac1766b18735ddc46103b1bb6c58f274b6c0c739
Cristian Rojas <crisdarroca@hotmail.com>
2022-12-26 21:31:23 -0500

    add: toggle task end add task

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { TaskCollection } from "./models/TaskCollection";
 import { tasks } from "./exampleData";
 
 const collection = new TaskCollection('Cristian', tasks)
+let showComplete: boolean = true
 
 const displayTaskList = (): void => {
-  // console.log(`${collection.userName}'s Tasks (${collection.getTaskCounts().inCompleted}) task to do`)
-  collection.getTaskItems(true).forEach(task => task.printDetails())
+  console.log(`${collection.userName}'s Tasks (${collection.getTaskCounts().inCompleted}) task to do`)
+  collection.getTaskItems(showComplete).forEach(task => task.printDetails())
 }
 displayTaskList()
 
@@ -15,7 +16,22 @@ enum Commands {
   Complete = "Complete Task",
   Toggle = "Show/Hide Completed",
   purge = 'Remove Completed Task',
-  Quit = 'Quitee'
+  Quit = 'Quite'
+}
+
+const promptAdd = async (): Promise<void> => {
+  console.clear()
+  const answers = await inquirer.prompt({
+    type: 'input',
+    name: 'add',
+    message: 'Enter task:'
+  })
+
+  if (answers.add !== '') {
+    collection.addTask(answers.add)
+  }
+
+  promptUser()
 }
 
 
@@ -23,13 +39,21 @@ const promptUser = async () => {
   console.clear()
   displayTaskList()
 
-  const answer = await inquirer.prompt({
+  const answers = await inquirer.prompt({
     type: 'list',
     name: 'command',
     message: 'Choose Option',
     choices: Object.values(Commands)
   })
 
-  console.log(answer)
+  switch (answers.command) {
+    case Commands.Toggle:
+      showComplete = !showComplete
+      promptUser()
+      break
+    case Commands.Add:
+      promptAdd()
+      break
+  }
 }
 promptUser()
